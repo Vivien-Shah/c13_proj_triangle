85ec56459eb1a123aaf85490438255aba77d553a
Vivien-Shah <hfsthane7787@gmail.com>
2025-07-25 17:34:17 +0530

    Add files via upload

---
+++ c13_proj_patterns_mirror_right_angled_triangle.py
@@ -0,0 +1,16 @@
+# Write a program to demonstrate a Mirrored Right Angled Triangle Pattern?
+
+'''totalrows=int(input("Enter the number of rows:"))
+for row in range (1, totalrows + 1): 
+
+    # Display Spaces
+
+    for space in range(1,(totalrows - row) + 1 ):
+         print(" ",end="")
+
+    # Display *
+
+    for symbol in range (1, row + 1):
+         print("*",end="")
+   
+    print()'''
